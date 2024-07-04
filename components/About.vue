c946b7cb5237931926bf726cd5d04a4a6bf142f5
favorodera <favorodera@gmail.com>
2024-07-04 19:38:02 +0000

    setup about section

--- components/About.vue
+++ components/About.vue
@@ -30,6 +30,72 @@
         </div>
       </div>
     </div>
+    <div class="about-container">
+      <div class="why-choose-container">
+        <div class="why-choose-heading-container">
+          <div class="why-choose-heading-line"><img src="../media/svg/line.svg" alt="line" /></div>
+          <p class="why-choose-heading">
+            Why Choose <span class="why-choose-heading-highlight">Scissor</span>
+          </p>
+        </div>
+        <p class="why-choose-text">
+          Scissor is the hub of everything that has to do with your link management. We shorten your
+          URLs, allow you creating custom ones for your personal, business, event usage. Our swift
+          QR code creation, management and usage tracking with advance analytics for all of these is
+          second to none.
+        </p>
+      </div>
+
+      <div class="features-container">
+        <div class="features-box-container">
+          <div class="feature-box">
+            <div class="feature-box-icon"><img src="../media/svg/about-link.svg" alt="link" /></div>
+            <div class="feature-box-text-container">
+              <p class="feature-box-text-header">URL Shortening</p>
+              <p class="feature-box-text">
+                Scissor allows you to shorten URLs of your business, events. Shorten your URL at
+                scale, URL redirects.
+              </p>
+            </div>
+          </div>
+
+          <div class="feature-box">
+            <div class="feature-box-icon"><img src="../media/svg/edit.svg" alt="link" /></div>
+            <div class="feature-box-text-container">
+              <p class="feature-box-text-header">Custom URLs</p>
+              <p class="feature-box-text">
+                With Scissor, you can create custom URLs, with the length you want! A solution for
+                socials and businesses.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class="features-box-container">
+          <div class="feature-box">
+            <div class="feature-box-icon"><img src="../media/svg/grid.svg" alt="link" /></div>
+            <div class="feature-box-text-container">
+              <p class="feature-box-text-header">QR Codes</p>
+              <p class="feature-box-text">
+                Generate QR codes to your business, events. Bring your audience and customers to
+                your doorstep with this scan and go solution.
+              </p>
+            </div>
+          </div>
+
+          <div class="feature-box">
+            <div class="feature-box-icon"><img src="../media/svg/activity.svg" alt="link" /></div>
+            <div class="feature-box-text-container">
+              <p class="feature-box-text-header">Data Analytics</p>
+              <p class="feature-box-text">
+                Receive data on the usage of either your shortened URL, custom URLs or generated QR
+                codes. Embedded to monitor progress.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
@@ -51,7 +117,7 @@ section {
   display: flex;
   align-items: center;
   align-content: center;
-  justify-content: center;
+  justify-content: space-around;
   gap: 2rem;
   background: #f9fbfd;
   width: 100%;
@@ -102,6 +168,101 @@ section {
 }
 
 .stat-text {
+  @include function.typography(1rem, 400, GilroyMedium, #141414, 1.5rem, start);
+}
+
+.about-container {
+  display: flex;
+  align-items: flex-start;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  gap: 4rem;
+  width: 100%;
+}
+
+.why-choose-container {
+  display: flex;
+  align-self: flex-start;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+  gap: 1rem;
+  width: 100%;
+  flex: 1 1 18.75rem;
+}
+
+.why-choose-heading-container {
+  display: flex;
+  gap: 1rem;
+  align-items: center;
+  justify-content: center;
+}
+
+.why-choose-heading-line {
+  @include function.mediaContainers(3rem, 0.00625rem);
+}
+
+.why-choose-heading {
+  @include function.typography(2.5rem, 400, GilroyBold, #141414, 2rem, start);
+}
+.why-choose-heading-highlight {
+  @include function.typography(2.5rem, 400, GilroyBold, #005ae2, 2rem, start);
+}
+.why-choose-text {
+  @include function.typography(1rem, 400, GilroyMedium, #141414, 1.75rem, start);
+  flex: 1 1 auto;
+}
+
+.features-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 4rem;
+  max-width: 44.25rem;
+  flex: 1 1 18.5rem;
+}
+
+.features-box-container {
+  display: flex;
+
+  align-items: flex-start;
+  justify-content: space-between;
+  gap: 1.75rem;
+  max-width: 100%;
+  flex: 1 1 18.5rem;
+}
+
+.feature-box {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  gap: 2.5rem;
+  max-width: 21.25rem;
+  flex: 1 1 18.5rem;
+}
+
+.feature-box-icon {
+  @include function.mediaContainers(3.5rem, 3.5rem);
+  border-radius: 100%;
+  background: #3284ff21;
+}
+
+.feature-box-text-container {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  gap: 1rem;
+  width: 100%;
+}
+
+.feature-box-text-header {
+  @include function.typography(2rem, 400, GilroySemiBold, #141414, 1.75rem, start);
+}
+
+.feature-box-text {
   @include function.typography(1rem, 400, GilroyMedium, #141414, 1.5rem, start);
 }
 </style>

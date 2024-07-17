9bde0f1392150b149192af954d05858935f9ee44
Favour Chidera Emeka <favorodera@gmail.com>
2024-07-17 11:52:46 +0100

    e2e test for Nav

---
+++ e2e/Nav.spec.ts
@@ -0,0 +1,34 @@
+import { test, expect } from '@playwright/test'
+
+test('Nav is rendered correctly', async ({ page }) => {
+  await page.goto('https://scissortool.web.app')
+
+  await expect(page).toHaveTitle('SCISSOR')
+
+  const nav = page.getByRole('navigation')
+  await expect(nav).toBeVisible()
+
+  const logoContainer = nav.locator('.logo-container')
+  await expect(logoContainer).toBeVisible()
+
+  const logo = logoContainer.locator('.logo')
+  await expect(logo).toBeVisible()
+
+  const logoImage = logo.getByAltText('logo')
+  await expect(logoImage).toBeVisible()
+
+  const logoLine = logoContainer.locator('.logo-line')
+  await expect(logoLine).toBeVisible()
+
+  const logoText = logoContainer.locator('.logo-text')
+  await expect(logoText).toBeVisible()
+  await expect(logoText).toHaveText('SCISSOR')
+
+  const navUrls = nav.getByRole('list')
+  await expect(navUrls).toBeVisible()
+  await expect(navUrls).toHaveClass('nav-urls')
+
+  const loginButton = nav.locator('.login')
+  await expect(loginButton).toBeVisible()
+  await expect(loginButton).toHaveText('Login')
+})
